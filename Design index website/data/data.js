4c833636bc232b51068421a2529bfb20ec5bc722
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-11-10 01:03:15 -0500

    cambios

---
+++ Design index website/data/data.js
@@ -0,0 +1,15 @@
+let data = {
+    "09/11/2023": {
+        "tareas": {
+            "url": "",
+            "hora": "18:00",
+            "tipo": "HTML/CSS"
+        },
+        "clase": {
+
+        }
+    }
+}
+
+// El orden se almacenará desde el más reciente al más antiguo.
+// Este "data.js" permitirá construir el sitio web con la información pertinente.
