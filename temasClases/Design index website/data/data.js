c8d087390a7c2665909a2a7268a215dcb1a4dbef
DavMateo <davidcarrenomc@gmail.com>
2024-03-28 14:50:30 -0500

    Pasando a carpetas, todos los archivos de las ramas anteriormente presentes en el repositorio. Por ende, se reestructura toda la organización de este repositorio.

---
+++ temasClases/Design index website/data/data.js
@@ -0,0 +1,40 @@
+let data = {
+    "09/11/2023": {
+        "tareas": {
+            "cantidadArchivos": 0,
+            "peso": 0,
+            "tipo": "",
+            "fecha": {
+                "creacion": "",
+                "modificacion": ""
+            }, //Se agrega al momento de que el usuario agregue la info. Se cargará la fecha con hora mediante JS
+            "imagenPreview": "",
+            "url": ""
+        },
+        "clase": {
+            "cantidadArchivos": 0,
+            "peso": 0,
+            "tipo": "",
+            "fecha": {
+                "creacion": "",
+                "modificacion": ""
+            },
+            "imagenPreview": "",
+            "url": ""
+        },
+        "refuerzo": {
+            "cantidadArchivos": 0,
+            "peso": 0,
+            "tipo": "",
+            "fecha": {
+                "creacion": "",
+                "modificacion": ""
+            },
+            "imagenPreview": "",
+            "url": ""
+        }
+    }
+}
+
+// El orden se almacenará desde el más reciente al más antiguo.
+// Este "data.js" permitirá construir el sitio web con la información pertinente.
